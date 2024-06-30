4f88f63c4ea935b6897cc4653aa61ba25e2d3043
chuyentran2511 <tchuyen23@gmail.com>
2024-06-30 17:53:18 +0800

    api

--- src/components/TitleCards/TitleCards.jsx
+++ src/components/TitleCards/TitleCards.jsx
@@ -1,14 +1,35 @@
-import React, { useEffect, useRef } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import './TitleCards.css'
 import cards_data from '../../assets/cards/Cards_data'
 const TitleCards = ({title, category}) => {
 
   const cardsRef = useRef();
+  const [apiData, setApiData] = useState([])
+
+  const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkMWIzMmM2MTY3MWQ0YjExYTg3OTJlYTkyMWU5NWM4YyIsIm5iZiI6MTcxOTczOTg1OS4wODA5MTUsInN1YiI6IjY2ODEyNDNhZDYwYzJlODQwOTlkYjI1OSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.dONeWNtTTUf7GIkqgJKDreT-DWqFZAcAdGcJivghN8Q'
+    }
+  };
+  
+  fetch('https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1', options)
+    .then(response => response.json())
+    .then(response => console.log(response))
+    .catch(err => console.error(err));
+
   const handleWheel = (event)=>{
     event.preventDefault();
     cardsRef.current.scrollLeft += event.deltaY;
   }
   useEffect(()=>{
+
+      fetch('https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1', options)
+    .then(response => response.json())
+    .then(response => setApiData(response.results))
+    .catch(err => console.error(err));
+
     cardsRef.current.addEventListener('wheel',handleWheel)
   },[])
   
@@ -18,11 +39,11 @@ const TitleCards = ({title, category}) => {
       {/* ref={cardsRef} để trong div cho ta tham chiếu đến phần tử đó 
       giúp ta tương tác với nó  */}
       <div className='card-list' ref={cardsRef}>
-        {cards_data.map((card,index)=>{
+        {apiData.map((card,index)=>{
           return(
             <div className='card' key={index}>
-              <img src={card.image} />
-              <p>{card.name}</p>
+              <img src={ `https://image.tmdb.org/t/p/w500/`+card.backdrop_path} />
+              <p>{card.original_title}</p>
             </div>
           )
         })}
